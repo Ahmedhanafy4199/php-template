40881b8da27736b2387fd564eead5f158c5a05b1
abdelghanycu <73040423+abdelghanycu@users.noreply.github.com>
2020-12-03 18:00:04 +0200

    Update EmployeeDao.php

--- backend/dao/EmployeeDao.php
+++ backend/dao/EmployeeDao.php
@@ -14,47 +14,28 @@ class EmployeeDao
 
         $connection = new mysqli($db_hostname, $db_username, $db_password, $db_database);
         if ($connection->connect_error) die($connection->connect_error);
-//        else echo 'Connected<br/>';
 
 
         $query = "SELECT * FROM employees";
         $result = $connection->query($query);
         if (!$result) die($connection->error);
-//        else echo 'Done<br/>';
 
         $arr_emp = array();
         $rows = $result->num_rows;
+
         for ($j = 0; $j < $rows; ++$j) {
+            $result->data_seek($j);
             $emp = new Employee();
-            $result->data_seek($j);
             $emp->setEmpNo($result->fetch_assoc()['emp_no']);
-            $result->data_seek($j);
-            $emp->setEmpNo($result->fetch_assoc()['birth_date']);
-            $result->data_seek($j);
-            $emp->setEmpNo($result->fetch_assoc()['first_name']);
-            $result->data_seek($j);
-            $emp->setEmpNo($result->fetch_assoc()['last_name']);
-            $result->data_seek($j);
-            $emp->setEmpNo($result->fetch_assoc()['gender']);
-            $result->data_seek($j);
-            $emp->setEmpNo($result->fetch_assoc()['hire_date']);
+            $emp->setBirthDate($result->fetch_assoc()['birth_date']);
+            $emp->setFirstName($result->fetch_assoc()['first_name']);
+            $emp->setLastName($result->fetch_assoc()['last_name']);
+            $emp->setGender($result->fetch_assoc()['gender']);
+            $emp->setHireDate($result->fetch_assoc()['hire_date']);
+
             $arr_emp[] = $emp;
-//            $result->data_seek($j);
-//            echo 'emp_no: ' . $result->fetch_assoc()['emp_no'] . '<br>';
-//            $result->data_seek($j);
-//            echo 'birth_date: ' . $result->fetch_assoc()['birth_date'] . '<br>';
-//            $result->data_seek($j);
-//            echo 'first_name: ' . $result->fetch_assoc()['first_name'] . '<br>';
-//            $result->data_seek($j);
-//            echo 'last_name: ' . $result->fetch_assoc()['last_name'] . '<br>';
-//            $result->data_seek($j);
-//            echo 'gender: ' . $result->fetch_assoc()['gender'] . '<br>';
-//            $result->data_seek($j);
-//            echo 'hire_date: ' . $result->fetch_assoc()['hire_date'] . '<br>';
-
-
-//            echo "*****************************************<br/>";
         }
+
         $result->close();
         $connection->close();
         return $arr_emp;
