96696d70e7287c35f36c81139498ced68791b269
abdelghanycu <73040423+abdelghanycu@users.noreply.github.com>
2020-12-13 19:31:22 +0200

    Update EmployeeService.php

--- backend/services/EmployeeService.php
+++ backend/services/EmployeeService.php
@@ -9,4 +9,19 @@ class EmployeeService
         $emp_dao = new EmployeeDao();
         return $emp_dao->getAll();
     }
+
+    public function create($emp){
+        $emp_dao = new EmployeeDao();
+        $emp_dao->create($emp);
+    }
+
+    public function update($emp){
+        $emp_dao = new EmployeeDao();
+        $emp_dao->update($emp);
+    }
+
+    public function delete($emp){
+        $emp_dao = new EmployeeDao();
+        $emp_dao->delete($emp);
+    }
 }
